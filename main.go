eecb409f54ac7abbffda5b90f29e761388923910
= <a186232641@gmail.com>
2025-08-03 06:54:30 +0800

    项目初始化

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+}
